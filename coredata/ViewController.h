4bbaf4b3ae79b93313e4b075475a56296041d569
admin <kuppala@ADMINs-MacBook-Air.local>
2017-01-20 09:25:23 +0530

    Initial Commit

---
+++ coredata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coredata
+//
+//  Created by siva bhargavi on 20/01/17.
+//  Copyright © 2017 siva bhargavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coredata/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coredata
+//
+//  Created by siva bhargavi on 20/01/17.
+//  Copyright © 2017 siva bhargavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
